20a988c304ba5a7b10bc7cea3a9840b5940b237a
Aloïs Deniel <alois.deniel@gmail.com>
2017-11-05 23:04:48 +0100

    Created lever.

---
+++ src/Hml.Parser/Lexing/HmlLexer.cs
@@ -0,0 +1,165 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hml.Parser
+{
+    public class HmlLexer
+    {
+        public HmlToken[] Tokenize(string content)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                writer.Write(content);
+                writer.Flush();
+                stream.Position = 0;
+                return this.Tokenize(stream);
+            }
+        }
+
+        public HmlToken[] Tokenize(Stream stream)
+        {
+            var result = new List<HmlToken>();
+            this.Tokenize(stream, (token) => result.Add(token));
+            return result.ToArray();
+        }
+
+        private StreamReader reader;
+
+        public void Tokenize(Stream stream, Action<HmlToken> onToken)
+        {
+            this.position = 0;
+
+            using(this.reader = new StreamReader(stream))
+            {
+                HmlToken token = null;
+
+                while (token?.Type != HmlTokenType.EndOfDocument)
+                {
+                    token = ProcessToken();
+                    onToken(token);
+                }
+            }
+        }
+
+        private int position;
+
+        private char? Read() 
+        {
+            if (reader.EndOfStream)
+                return null;
+
+            position++;
+            return (char)reader.Read();
+        }
+
+        private char? Peek() => reader.EndOfStream ? null : (char?)reader.Peek();
+
+        private void SkipWhitespaces()
+        {
+            char? c;
+            while((c = this.Peek()) == ' ')
+            {
+                this.Read();
+            }
+        }
+
+        private HmlToken ProcessToken()
+        {
+            var start = this.position;
+
+            var currentOrEnd = this.Read();
+
+            if(currentOrEnd == null)
+            {
+                return new HmlToken(HmlTokenType.EndOfDocument, start, start, null);
+            }
+
+            HmlTokenType type;
+            string content;
+
+            var current = (char)currentOrEnd;
+            content = current.ToString();
+
+            switch (current)
+            {
+                case var letter when char.IsLetter(current):
+                case '_':
+                    type = HmlTokenType.Identifier;
+                    var idBuilder = new StringBuilder();
+                    idBuilder.Append(current);
+                    char? next;
+                    while ((next = this.Peek()) != null && (
+                        char.IsLetter(next ?? '0') ||
+                          char.IsDigit(next ?? 'A') ||
+                          next == '.' || 
+                          next == '-' ||
+                          next == '_'))
+                    {
+                        idBuilder.Append(current = (char)this.Read());
+                    }
+                    content = idBuilder.ToString();
+                    break;
+                case ' ':
+                    type = HmlTokenType.Whitespaces;
+                    var wsBuilder = new StringBuilder();
+                    while ((next = this.Peek()) == ' ')
+                    {
+                        wsBuilder.Append(current = (char)this.Read());
+                    }
+                    content = wsBuilder.ToString();
+                    break;
+                case '\n':
+                case '\r':
+                    type =  HmlTokenType.LineReturn;
+                    break;
+                case '=':
+                    type =  HmlTokenType.Equals;
+                    break;
+                case '"':
+                    type =  HmlTokenType.PropertyValue;
+                    var builder = new StringBuilder();
+                    while((currentOrEnd = this.Read()) != null && currentOrEnd != '"')
+                    {
+                        // Escape "
+                        if(currentOrEnd == '\\' && (next = this.Peek()) == '"')
+                        {
+                            currentOrEnd = this.Read();
+                        }
+                        
+                        builder.Append(currentOrEnd);
+                    }
+                    content = builder.ToString();
+                    break;
+                case ':':
+                    type = HmlTokenType.Text;
+                    this.SkipWhitespaces();
+                    var textBuilder = new StringBuilder();
+                    while ((next = this.Peek()) != null && next != '\n' && next != '\r')
+                    {
+                        textBuilder.Append(currentOrEnd = this.Read());
+                    }
+                    content = textBuilder.ToString();
+                    break;
+                case '(':
+                    type = HmlTokenType.PropertiesStart;
+                    break;
+                case ')':
+                    type = HmlTokenType.PropertiesEnd;
+                    break;
+                case ',':
+                    type = HmlTokenType.PropertiesSeparator;
+                    break;
+                default:
+                    type = HmlTokenType.Unknown;
+                    break;
+            }
+
+            var end = this.position;
+
+            return new HmlToken(type, start, end - start, content);
+        }
+    }
+}
---
+++ src/Hml.Tests/LexerTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using NUnit.Framework;
+using Hml.Parser;
+using System.Linq;
+
+namespace Hml.Tests
+{
+    [TestFixture()]
+    public class LexerTests
+    {
+        private HmlLexer lexer;
+
+        [SetUp]
+        public void Initialize()
+        {
+            this.lexer = new HmlLexer();
+        }
+
+        [TearDown]
+        public void Destroy()
+        {
+            this.lexer = null;
+        }
+
+        [Test]
+        public void Tokenize_ValidSequence_Succeed()
+        {
+            var hml = @"node.test (prop=""v"", other=""otherv""): the value text of the element
+  other_1";
+
+            var tokens = this.lexer.Tokenize(hml);
+
+            Assert.AreEqual(HmlTokenType.Identifier, tokens[0].Type);
+            Assert.AreEqual("node.test", tokens[0].Content);
+            Assert.AreEqual(0, tokens[0].Start);
+            Assert.AreEqual(9, tokens[0].Length);
+
+            Assert.AreEqual(HmlTokenType.Whitespaces, tokens[1].Type);
+
+            Assert.AreEqual(HmlTokenType.PropertiesStart, tokens[2].Type);
+
+            Assert.AreEqual(HmlTokenType.Identifier, tokens[3].Type);
+            Assert.AreEqual("prop", tokens[3].Content);
+
+            Assert.AreEqual(HmlTokenType.Equals, tokens[4].Type);
+
+            Assert.AreEqual(HmlTokenType.PropertyValue, tokens[5].Type);
+            Assert.AreEqual("v", tokens[5].Content);
+
+            Assert.AreEqual(HmlTokenType.PropertiesSeparator, tokens[6].Type);
+
+            Assert.AreEqual(HmlTokenType.Whitespaces, tokens[7].Type);
+
+            Assert.AreEqual(HmlTokenType.Identifier, tokens[8].Type);
+            Assert.AreEqual("other", tokens[8].Content);
+
+            Assert.AreEqual(HmlTokenType.Equals, tokens[9].Type);
+
+            Assert.AreEqual(HmlTokenType.PropertyValue, tokens[10].Type);
+            Assert.AreEqual("otherv", tokens[10].Content);
+
+            Assert.AreEqual(HmlTokenType.PropertiesEnd, tokens[11].Type);
+
+            Assert.AreEqual(HmlTokenType.Text, tokens[12].Type);
+            Assert.AreEqual("the value text of the element", tokens[12].Content);
+
+            Assert.AreEqual(HmlTokenType.LineReturn, tokens[13].Type);
+
+            Assert.AreEqual(HmlTokenType.Whitespaces, tokens[14].Type);
+
+            Assert.AreEqual(HmlTokenType.Identifier, tokens[15].Type);
+            Assert.AreEqual("other_1", tokens[15].Content);
+
+            Assert.AreEqual(HmlTokenType.EndOfDocument, tokens[16].Type);
+        }
+
+        [Test]
+        public void Tokenize_EscapedChar_Succeed()
+        {
+            var hml = @"""test\""test""";
+
+            var tokens = this.lexer.Tokenize(hml);
+
+            Assert.AreEqual(HmlTokenType.PropertyValue, tokens[0].Type);
+            Assert.AreEqual("test\"test", tokens[0].Content);
+        }
+    }
+}
